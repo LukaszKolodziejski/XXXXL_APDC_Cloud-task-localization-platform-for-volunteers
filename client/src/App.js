0ebbe063e4dbe50ff1f487c58fd2cd477cb76ee7
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-05-21 13:12:11 +0100

    set first configuration, instal packages

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">To jest mój kkk projekt testowy.</header>
+    </div>
+  );
+}
+
+export default App;
