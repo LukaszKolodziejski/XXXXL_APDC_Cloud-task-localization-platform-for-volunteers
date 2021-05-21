c46dc2df1ff94e608b54afe3ad79b5df30eff60a
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-05-21 19:16:20 +0100

    build main structur of App.js & index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Layout from "./hoc/Layout/Layout";
+import TasksMap from "./containers/TasksMap/TasksMap";
+// import Auth from "./containers/Auth/Auth";
+// import User from "./containers/User/User";
+// import Accounts from "./containers/Accounts/Accounts";
+// import Logout from "./containers/Auth/Logout/Logout";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">To jest mój kkk projekt testowy.</header>
-    </div>
+    <Router>
+      <Layout>
+        <Route path="/" exact component={TasksMap} />
+        {/* <Route path="/user" component={User} /> */}
+        {/* <Route path="/accounts" component={Accounts} /> */}
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
