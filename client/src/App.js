47d9930e0cd9708a1ec5779db66570598018f92d
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-05-24 00:33:08 +0100

    add ranking component to App

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import TasksMap from "./containers/TasksMap/TasksMap";
+import Ranking from "./containers/Ranking/Ranking";
 // import Auth from "./containers/Auth/Auth";
 // import User from "./containers/User/User";
 // import Accounts from "./containers/Accounts/Accounts";
@@ -12,6 +13,7 @@ const App = () => {
     <Router>
       <Layout>
         <Route path="/" exact component={TasksMap} />
+        <Route path="/ranking" exact component={Ranking} />
         {/* <Route path="/user" component={User} /> */}
         {/* <Route path="/accounts" component={Accounts} /> */}
       </Layout>
