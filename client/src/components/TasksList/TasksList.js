58c00691cd420a317d5c6ecf03b2b77ead730ddf
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-05-23 20:02:01 +0100

    change data sended to TaskList

--- client/src/components/TasksList/TasksList.js
+++ client/src/components/TasksList/TasksList.js
@@ -1,15 +1,31 @@
 import React from "react";
 import styles from "./TasksList.module.css";
+import ListTasksHeader from "./ListTasksHeader/ListTasksHeader";
+import SingleTask from "./SingleTask/SingleTask";
+import tasks from "./tasks.json";
 
 const TasksList = (props) => {
+  const addTask = () => {
+    const date = new Date();
+    date.setDate(date.getDate() + 2);
+    console.log(date);
+  };
+
+  const listOfTasks = tasks.map((task) => (
+    <SingleTask
+      key={task.id}
+      id={task.id}
+      name={task.name}
+      status={task.status}
+      expiryDate={task.expiryDate}
+      coins={task.coins}
+    />
+  ));
+
   return (
     <section className={styles.TasksList}>
-      <div>
-        <div>Nick</div>
-        <div>Go out with dog.</div>
-        <div>data</div>
-        <div>10 Coin</div>
-      </div>
+      <ListTasksHeader />
+      <div>{listOfTasks}</div>
     </section>
   );
 };
