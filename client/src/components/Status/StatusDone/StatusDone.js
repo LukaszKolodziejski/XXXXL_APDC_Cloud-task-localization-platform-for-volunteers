e4256608904ad5280667b681ce83103bfd52aef8
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-05-24 00:44:19 +0100

    add done

---
+++ client/src/components/Status/StatusDone/StatusDone.js
@@ -0,0 +1,19 @@
+import React, { useRef, useEffect } from "react";
+import PropTypes from "prop-types";
+import gsap from "gsap";
+
+import styles from "./StatusDone.module.css";
+
+const StatusDone = React.memo((props) => {
+  return (
+    <div className={styles.Container}>
+      <div className={styles.StatusDone}>{props.status}</div>
+    </div>
+  );
+});
+
+StatusDone.propTypes = {
+  status: PropTypes.string.isRequired,
+};
+
+export default StatusDone;
