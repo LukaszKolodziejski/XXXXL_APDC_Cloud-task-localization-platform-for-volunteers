c95ca6cd33f028be3f148a99c0cb5e4e4726e07b
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-05-24 18:03:17 +0100

    add page New Tasks

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import TasksMap from "./containers/TasksMap/TasksMap";
 import Ranking from "./containers/Ranking/Ranking";
+import NewTasksMap from "./containers/NewTasksMap/NewTasksMap";
 // import Auth from "./containers/Auth/Auth";
-// import User from "./containers/User/User";
-// import Accounts from "./containers/Accounts/Accounts";
 // import Logout from "./containers/Auth/Logout/Logout";
 
 const App = () => {
@@ -14,8 +13,7 @@ const App = () => {
       <Layout>
         <Route path="/" exact component={TasksMap} />
         <Route path="/ranking" exact component={Ranking} />
-        {/* <Route path="/user" component={User} /> */}
-        {/* <Route path="/accounts" component={Accounts} /> */}
+        <Route path="/new-task" exact component={NewTasksMap} />
       </Layout>
     </Router>
   );
