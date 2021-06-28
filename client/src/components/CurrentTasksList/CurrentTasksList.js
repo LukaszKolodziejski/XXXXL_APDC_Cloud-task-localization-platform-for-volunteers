b2a76933d508cb6d5b9c08cc2fb6baef81a91f2b
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2021-06-28 02:14:02 +0100

    current task list

---
+++ client/src/components/CurrentTasksList/CurrentTasksList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./CurrentTasksList.module.css";
+import CurrentSingleTask from "./CurrentSingleTask/CurrentSingleTask";
+import TopHeader from "../UX/TopHeader/TopHeader";
+
+const CurrentTasksList = (props) => {
+  const headerNames = ["# Number", "LOCATION", "DESCRIPTION", "COINS"];
+
+  const listOfTasks = props.newTasksList.map((task) => (
+    <CurrentSingleTask
+      key={task.id}
+      id={task.id}
+      coins={task.coins}
+      location={task.location}
+      description={task.description}
+      onDescription={props.descriptionHandler}
+      onActiveDataList={props.onActiveDataList}
+    />
+  ));
+
+  const Info =
+    listOfTasks.length === 0 ? (
+      <div className={styles.Info}>Click on the map to add tasks.</div>
+    ) : null;
+
+  return (
+    <section className={styles.CurrentTasksList}>
+      <TopHeader styles="NewTasks" names={headerNames} />
+      {Info}
+      <div>{listOfTasks}</div>
+      {listOfTasks.length ? props.children : null}
+    </section>
+  );
+};
+
+export default CurrentTasksList;
